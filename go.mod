4947c97cf1a4d87fbe99bccd194c13afa7b697ec
locker98 <coolemail45@proton.me>
2025-09-18 07:04:46 -0400

    Added System Tray

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/Endg4meZer0/go-mpris v1.0.5
+	github.com/getlantern/systray v1.2.2
 	github.com/godbus/dbus/v5 v5.1.0
 	github.com/jfreymuth/pulse v0.1.1
 	github.com/sahilm/fuzzy v0.1.1
@@ -13,4 +14,14 @@ require (
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/sys v0.8.0 // indirect
+require (
+	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
+	github.com/getlantern/errors v0.0.0-20190325191628-abdb3e3e36f7 // indirect
+	github.com/getlantern/golog v0.0.0-20190830074920-4ef2e798c2d7 // indirect
+	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
+	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
--- pkg/mixmaster/cmd/main.go
+++ pkg/mixmaster/cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"gitea.locker98.com/locker98/Mixmaster/pkg/mixmaster"
+	"gitea.locker98.com/locker98/Mixmaster/pkg/mixmaster/icon"
+	"github.com/getlantern/systray"
 )
 
 var (
@@ -21,6 +23,41 @@ func main() {
 		return
 	}
 
-	mixmaster.NewMixMaster(configFile)
+	systray.Run(onReady, onExit)
 
+}
+
+func onReady() {
+	systray.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetTitle("MixMaster")
+	config := systray.AddMenuItem("Config", "Configure Settings")
+	systray.AddSeparator()
+	check := systray.AddMenuItemCheckbox("Check", "Please Check Me", false)
+	systray.AddSeparator()
+	quit := systray.AddMenuItem("Quit", "Stop deej and quit")
+
+	go mixmaster.NewMixMaster(configFile)
+
+	for {
+		select {
+		case <-quit.ClickedCh:
+			systray.Quit()
+
+		case <-config.ClickedCh:
+			fmt.Println("Config Clicked")
+
+		case <-check.ClickedCh:
+			if check.Checked() {
+				check.Uncheck()
+				fmt.Println("Unchecked")
+			} else {
+				check.Check()
+				fmt.Println("Checked")
+			}
+		}
+	}
+}
+
+func onExit() {
+	fmt.Println("Exit")
 }
